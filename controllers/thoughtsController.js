1f8b6ba48f2f3eed2262949f7b8ab3d34f0e65c6
Slender James <114044205+jperalta24@users.noreply.github.com>
2023-03-25 15:19:09 -0700

    add reaction function

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -26,7 +26,9 @@ module.exports = {
     //create a thought
     createThought: async function createThought(req, res) {
         try {
+            //create a new thought and store it in the new thought variable.
             const newThought = await Thought.create(req.body)
+            // find user with the matching userid specified in the req.body 
             const user = await User.findById(req.body.userId)
             //dot notation to push the thoughts id into the users thoughts array field
             user.thoughts.push(newThought._id)
@@ -34,6 +36,50 @@ module.exports = {
             res.status(200).json(newThought);
         } catch (err) {
             res.status(500).json(err)
+        }
+    },
+
+    updateThought: async function updateThought(req, res) {
+        try {
+            const updateThought = await Thought.findByIdAndUpdate(
+                req.params.id,
+                { $set: req.body },
+                { runValidators: true, new: true }
+            )
+            !updateThought ? res.status(404).json('Cannot find thought with this id') : res.json(updateThought)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+
+    deleteThought: async function deleteThought(req, res) {
+        try {
+            const deleteThought = await Thought.findByIdAndDelete(req.params.id);
+            !deleteThought ? res.status(404).json('thought not found') : res.status(200).json('thought has been successfully deleted')
+
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+
+    addReaction: async function addReaction(req, res) {
+        try {
+            const newReaction = await Thought.findByIdAndUpdate(
+                req.params.id,
+                {$addToSet: {reactions: req.body}},
+                { runValidators: true, new: true }
+                )
+                !newReaction ? res.status(404).json('Cannot find thought') : res.status(200).json(newReaction)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+
+    deleteReaction: async function deleteReaction(req, res) {
+        try {
+            const deleteReaction = await Thought.findByIdAndUpdate(req.params.id)
+        } catch (err) {
+            
         }
     }
 
--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -3,6 +3,9 @@ const {
 getThoughts,
 getSingleThought,
 createThought,
+updateThought,
+deleteThought,
+addReaction,
 
 } = require('../../controllers/thoughtsController');
 
@@ -12,6 +15,11 @@ router.route('/')
 
 router.route('/:id')
 .get(getSingleThought)
+.put(updateThought)
+.delete(deleteThought)
 
+router.route('/thoughtId/reactions')
+.post(addReaction)
+.delete(deleteReaction)
 
 module.exports = router;
